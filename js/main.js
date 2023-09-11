244748912ccb4a6248e625aaf027347e4637809d
usagi0000 <141452061+usagi0000@users.noreply.github.com>
2023-09-11 08:49:07 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* GSAP ANIMATION */
+gsap.from('.sa', 1.2,{opacity: 0, y: 200,delay: .1});
+gsap.from('.presentation h1', 1.2,{opacity: 0, x: -100,delay: .1});
+gsap.from('.down ', 1.2,{opacity: 0, y: -150,delay: .1});
+
+gsap.from('.heart1', 1.2,{opacity: 0, y: -200,delay: .1});
+gsap.from('.heart2', 1.2,{opacity: 0, y: -200,delay: .2});
+gsap.from('.heart3', 1.2,{opacity: 0, y: -200,delay: .3});
+gsap.from('.heart4', 1.2,{opacity: 0, y: -200,delay: .4});
+gsap.from('.heart5', 1.2,{opacity: 0, y: -200,delay: .5});
+gsap.from('.heart6', 1.2,{opacity: 0, y: -200,delay: .6});
+
+
+
+
