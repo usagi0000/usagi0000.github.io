4cd053f27ec1ef7dc86ffc6b8f778d292fc3fd06
usagi0000 <141452061+usagi0000@users.noreply.github.com>
2023-09-24 04:17:37 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,15 +2,46 @@
 gsap.from('.sa', 1.2,{opacity: 0, y: 200,delay: .1});
 gsap.from('.presentation h1', 1.2,{opacity: 0, x: -100,delay: .1});
 gsap.from('.down ', 1.2,{opacity: 0, y: -150,delay: .1});
+gsap.from('.contactImg ', 1.2,{opacity: 0, y: 150,delay: .1});
+gsap.from('.forum ', 1.2,{opacity: 0, x: 150,delay: .1});
 
-gsap.from('.heart1', 1.2,{opacity: 0, y: -200,delay: .1});
-gsap.from('.heart2', 1.2,{opacity: 0, y: -200,delay: .2});
-gsap.from('.heart3', 1.2,{opacity: 0, y: -200,delay: .3});
-gsap.from('.heart4', 1.2,{opacity: 0, y: -200,delay: .4});
-gsap.from('.heart5', 1.2,{opacity: 0, y: -200,delay: .5});
-gsap.from('.heart6', 1.2,{opacity: 0, y: -200,delay: .6});
 
-gsap.from('.diplome ', 1.2,{opacity: 0, y: -200,delay: 1});
+
+gsap.from('.diplome ', 1.2,{opacity: 0, y: -200,delay: .1});
+
+var icon=document.getElementById("icon");
+var line=document.getElementById("line");
+var line2=document.getElementById("line2");
+var theme;
+icon.onclick=function(){
+    document.body.classList.toggle("dark-theme");
+}
+icon.onclick=function(){
+    if(document.body.classList.toggle("dark-theme")){
+        icon.src="images/sun.png";
+        line.src="images/Line 2.png";
+        line2.src="images/Line 2.png";
+        theme="dark";
+        localStorage.setItem("mode", "dark")  
+
+    }else{
+        icon.src="images/moon.png";
+        line.src="images/Line 1.png";
+        line2.src="images/Line 1.png";
+        theme="light";
+
+    } localStorage.setItem("PageTheme", JSON.stringify(theme));
+}
+
+    let GetTheme = JSON.parse(localStorage.getItem("PageTheme"));
+    console.log(GetTheme);
+    if(GetTheme === "dark"){
+        document.body.classList = "dark-theme";
+    }else{
+        document.body.classList = "";
+    }
+
+
 
 
 
